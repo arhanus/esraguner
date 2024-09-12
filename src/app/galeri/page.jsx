2d22a3e959b7e5e0ebbca4a6eef28db1ff5d3c43
arhanus <140638059+arhanus@users.noreply.github.com>
2024-09-13 00:46:48 +0300

    Büyük dosyalar LFS ile eklendi

--- src/app/galeri/page.jsx
+++ src/app/galeri/page.jsx
@@ -1,11 +1,10 @@
-import Galeri from "@/components/Galeri";
-import React from "react";
+
 
 const page = () => {
   return (
     <div className="flex flex-col gap-8 pt-36 min-h-screen w-full bg-zinc-900 items-center justify-center">
       <h1 className="font-semibold text-5xl tracking-widest text-zinc-300">GALERİ</h1>
-      <Galeri />
+      
     </div>
   );
 };
