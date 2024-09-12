c5827f85ba130a7fea6dba2e91e94ffd77c6b126
arhanus <140638059+arhanus@users.noreply.github.com>
2024-09-13 00:29:42 +0300

    blobs

--- src/components/Galeri.jsx
+++ src/components/Galeri.jsx
@@ -1,30 +1,14 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-
-const Galeri = () => {
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-   
-    const videoFiles = [
-      'video1.mp4',
-      'video2.mp4',
-      'video3.mp4',
-      'video5.mp4',
-      'video6.mp4',
-      'video7.mp4',
-      'video8.mp4',
-
-    ];
-    setVideos(videoFiles);
-  }, []);
-
+import { list } from '@vercel/blob';
+const Galeri = async() => {
+  
+  const {blobs} = await list();
+  console.log(blobs)
   return (
     <div className="w-full md:w-2/4 mx-auto min-h-full py-20 bg-zinc-900">
-      {videos.map((video, index) => (
-        <div key={index} className={`w-full flex ${index % 2 === 0 ? 'justify-start' : 'justify-end'}` }>
+      {blobs.map((blob, index) => (
+        <div key={blob.url} className={`w-full flex ${index % 2 === 0 ? 'justify-start' : 'justify-end'}` }>
           <video width="320" height="240" controls>
-            <source src={`/${video}`} type="video/mp4" />
+            <source src={blob.url} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
